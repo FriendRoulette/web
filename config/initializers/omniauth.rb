1f7021b604c28dd118a9b5d9d1157e868d4ad250
Andrew Page <andrew@andrewpage.me>
2014-04-12 09:24:58 -0700

    Reset secret key

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '229792727217503', 'cecb734ba75df3528ce5d3dd60e7f3ba', {scope: 'email, read_stream, read_friendlists, friends_likes, friends_status, offline_access'}
+  provider :facebook, '229792727217503', 'eca953142e00aa1b538e59739805e131', {scope: 'email, read_stream, read_friendlists, friends_likes, friends_status, offline_access'}
 end
