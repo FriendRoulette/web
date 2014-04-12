8a5a51b06e5384381a11cb7f3fed10dda173ea90
Andrew Page <andrew@andrewpage.me>
2014-04-12 10:28:15 -0700

    AUthentication controller

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -1,18 +1,27 @@
 class AuthenticationsController < ApplicationController
 	def create
+		puts "Authentication CREATE!"
+
 		auth = request.env["omniauth.auth"]
+
+		puts 'Auth: ' + auth
 
 	    # Try to find authentication first
 	    authentication = Authentication.find_by_provider_and_uid(auth['provider'], auth['uid'])
 
 		if authentication
+			puts 'Auth exists'
 		    # Authentication found, sign the user in.
 		    flash[:notice] = "Signed in successfully."
 		    sign_in_and_redirect(:user, authentication.user)
 	    else
+	    	puts 'Auth does not exist'
 		    # Authentication not found, thus a new user.
 		    user = User.new
 		    user.apply_omniauth(auth)
+
+		    puts 'apply omniauth'
+
 		    if user.save(:validate => false)
 		    	flash[:notice] = "Account created and signed in successfully."
 		    	sign_in_and_redirect(:user, user)
