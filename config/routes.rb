8fe9fd3458cecc9be10123c0aab2ac70d4c80c1e
Andrew Page <andrew@andrewpage.me>
2014-04-12 11:15:02 -0700

    Logout path

--- config/routes.rb
+++ config/routes.rb
@@ -10,13 +10,14 @@ Rails.application.routes.draw do
  		end
  	end
 
- 	devise_for :users
+ 	devise_for :users do
+ 		get "logout" => "devise/sessions#new"
+ 	end
 
 	match '/auth/:provider/callback', to: 'authentications#create', via: [:get, :post]
 	match 'auth/failure', to: redirect('/'), via: [:get, :post]
 
 	match 'welcome', to: 'pages#welcome', as: 'welcome', via: [:get]
-	match 'logout', to: destroy_user_session_path, as: 'logout', via: [:get, :post]
 
 	root to: 'pages#home'
 end
