059a7a1c79443d29a9269d5dd609a529e183ab8d
Andrew Page <andrew@andrewpage.me>
2014-04-12 04:15:52 -0700

    Beginnings of JSON API

--- app/controllers/api/accounts_controller.rb
+++ app/controllers/api/accounts_controller.rb
@@ -1,2 +1,26 @@
 class Api::AccountsController < ApplicationController
+	def index
+		@accounts = Account.all
+
+		respond_to do |format|
+			format.json { render json: @accounts }
+		end
+	end
+
+	def show
+		@account = Account.find(params[:id])
+
+		respond_to do |format|
+			format.json { render json: @account }
+		end
+	end
+
+	def create
+	end
+
+	def update
+	end
+
+	def destroy
+	end
 end
