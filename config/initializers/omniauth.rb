6e2e024defc3cc1879f1ad8720199f8ec7e19d7e
Andrew Page <andrew@andrewpage.me>
2014-04-12 09:38:33 -0700

    Changed API key

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '541756572610649', 'eca953142e00aa1b538e59739805e131', {scope: 'email, read_stream, read_friendlists, friends_likes, friends_status, offline_access'}
+  provider :facebook, Settings.apis.facebook.api_key, Settings.apis.facebook.api_secret, {scope: 'email, read_stream, read_friendlists, friends_likes, friends_status, offline_access'}
 end
