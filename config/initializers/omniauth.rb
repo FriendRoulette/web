73ff3b42d0f283051ca63a2fdfade98a410e348a
Andrew Page <andrew@andrewpage.me>
2014-04-12 11:47:16 -0700

    Omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,6 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, Settings.apis.facebook.api_key, Settings.apis.facebook.api_secret, {scope: 'email, read_stream, read_friendlists, friends_likes, friends_status, offline_access'}
+  provider :facebook, Settings.apis.facebook.api_key, Settings.apis.facebook.api_secret, { scope: Settings.apis.facebook.access_requirements }
 end
+
