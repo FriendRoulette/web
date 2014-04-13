70fe399e60981757a7cf981e1cf0c4ad0e04983e
Marcus Smith <mail4marcus@comcast.net>
2014-04-12 18:22:10 -0700

    updated scope

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '541756572610649', '84742819c9b850eb9173e5f80b0367b8', {scope: 'email, read_stream, read_friendlists, friends_likes, friends_status, offline_access'}
+  provider :facebook, '229792727217503', 'cecb734ba75df3528ce5d3dd60e7f3ba', {scope: 'basic_info, email, read_stream, read_friendlists, user_birthday, friends_likes, friends_status, offline_access'}
 end
