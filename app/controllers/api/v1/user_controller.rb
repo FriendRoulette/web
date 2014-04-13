9c45dce45b1514ab875fb9a62326bdcd689ff3d7
Marcus Smith <mail4marcus@comcast.net>
2014-04-13 08:52:30 -0700

    Debugging

--- app/controllers/api/v1/user_controller.rb
+++ app/controllers/api/v1/user_controller.rb
@@ -37,40 +37,12 @@ module Api
 				@user = User.new(user_params)
 				p "IN USER CREATE"
 				p @user
-				p @user["id"]
-				# if (@user["subdomain"] == "api")
-					p "Create Oauth"
-					auth = params['oauth']
-					p "auth"
-					p auth
-					existing = User.find_by_token(auth)
-					p "existing"
-					p existing
-					if !existing.nil?
-						render json: existing
-					else			
-						graph = Koala::Facebook::API.new(token)
-						profile = graph.get_object("me")
-
-					    id = profile["id"]
-					    name = profile["name"]
-					    email = profile["email"]
-
-				 	   user = User.new(uid: id, name: name, email: email, oauth_token: auth)
-
-				  		if user.save
-					    	render json: user
-				  	 	else
-				    		render json: user.errors.full_messages
-				    	end
-					end
-				# else
-				# 	if @user.save
-				# 		render json: @user
-				# 	else
-				# 		render json: { status: false }
-				# 	end
-				# end
+				p @user_params
+				if @user.save
+					render json: @user
+				else
+					render json: { status: false }
+				end
 			end
 
 			def create_oauth
